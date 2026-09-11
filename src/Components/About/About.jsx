9813e33b14725eb924621d7882107ab2c3b93cf9
Harshit Kumar <hkharshit1998@gmail.com>
2026-09-11 11:08:38 +0530

    update data

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-            <h1>4.10+</h1>
+            <h1>5.2+</h1>
             <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div id='home' className='hero'>
       <img src={profile_img} alt="" className='profile-img' />
       <h1><span>I'm Harshit Kumar,</span> Dotnet Developer based in India.</h1>
-      <p>I am a seasoned .NET developer with 4.10 years of experience in the industry, having had the privilege of contributing to Product Based Companies Like : <a href="https://www.gingerwebs.com/" target="_blank">Visit</a>.</p>
+      <p>I am a seasoned .NET developer with 5.2+ years of experience in the industry, having had the privilege of contributing to Product Based Companies Like : <a href="https://www.gingerwebs.com/" target="_blank">Visit</a>.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">My Resume</div>
